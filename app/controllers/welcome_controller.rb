5c4addf9d188a7493e66835b9030cb10aecb5522
dmitriybuzina <dmitry.buzina@faceit-team.com>
2019-01-17 18:11:43 +0200

    views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @images = Image.order('created_at DESC').limit(5)
+    @images = Image.order("RANDOM()").limit(8)
     @categories = Category.order('created_at DESC').limit(4)
     @categories.each do |category|
       preview(category)
